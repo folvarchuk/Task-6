e8178d298458e92ff2faf3f92b12207549779738
andrey <andrey@0D6C>
2021-12-05 20:00:17 +0300

    Добавьте файлы проекта.

---
+++ Задание 6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. Знаки препинания не используются. Найти самое длинное слово в строке.*/
+
+            Console.WriteLine("Введите предложение без использования знаков препинания:");
+            string sentence = Console.ReadLine();
+            //Разбиваем предложение на слова
+            string[] sentenceArray = sentence.Split();
+            //Посчитаем количество слов в предложении:
+            int count = 0;
+            foreach(string s in sentenceArray)
+            {
+                count++;
+            }
+            //Ищем максимальное количество символов в каждом слове:
+            int max = sentenceArray[0].Length;
+            for(int i = 0; i < count; i++)
+            {
+                if (max < sentenceArray[i].Length)
+                    max = sentenceArray[i].Length;
+            }
+            //Выводим слово или слова с наибольшим количеством символов:
+            Console.WriteLine("Вот самое длинное слово или самые длинные слова в предложении:");
+            foreach(string k in sentenceArray)
+            {
+                if(k.Length == max)
+                    Console.WriteLine("{0}, в нём {1} символов", k, max);
+            }    
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задание 6_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. 
+             *Знаки препинания не используются. Составить программу, определяющую является ли строка палиндромом
+             *без учёта пробелов и регистра (пример палиндрома – «А роза упала на лапу Азора»).*/
+
+            Console.WriteLine("Введите предложение без использования знаков препинания:");
+            string sentence = Console.ReadLine();
+            //Разбиваем предложение на слова
+            string[] sentenceArray = sentence.Split();
+            //Приводим все символы в предложении к одному регистру (нижнему) и удаляем пробелы между словами:
+            string resultSentence = "";
+            foreach (string s in sentenceArray)
+                resultSentence += s.ToLower();
+            //Проверяем является ли строка палиндромом.
+            //Разбиваем получившуюся строку без пробелов на две половины.
+            string firstHalfOfSentence = "";
+            string secondHalfOfSentence = "";
+            for (int i = 0; i <= resultSentence.Length / 2; i++)
+                firstHalfOfSentence += resultSentence[i];
+            for (int j = resultSentence.Length - 1; j >= resultSentence.Length / 2; j--)
+                secondHalfOfSentence += resultSentence[j];
+            if (firstHalfOfSentence == secondHalfOfSentence)
+                Console.WriteLine("Палиндром!");
+            else
+                Console.WriteLine("Не палиндром!");
+            Console.ReadKey();
+        }
+    }
+}
